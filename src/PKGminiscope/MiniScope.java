b2946ae562a8865837f17ea77c31e9111ba46e98
Dreaguns <jcr.engineer@gmail.com>
2018-11-22 08:26:32 -0500

    add constructor for MiniScope Class
    Signed-off-by: Dreaguns <jcr.engineer@gmail.com>

--- src/PKGminiscope/MiniScope.java
+++ src/PKGminiscope/MiniScope.java
@@ -9,5 +9,14 @@ package REMtracker.src.PKGminiscope;
  * @version 1.0
  * @since   2018-NOV-20
  */
+
 public class MiniScope {
+
+    //Constructor with no arguments instantiates associative classes
+    MiniScope(){
+        MiniScopeDataModel miniScopeDataModel = new MiniScopeDataModel();
+        MiniScopeTimerTask miniScopeTimerTask = new MiniScopeTimerTask();
+        MiniScopeEventRelayer miniScopeEventRelayer = new MiniScopeEventRelayer();
+        Channel scopeChannel = new Channel();
+    }
 }
