368ffa3553e5f0b1aa6b861a2fb4d458a41e540d
Dreaguns <jcr.engineer@gmail.com>
2018-11-29 21:24:05 -0500

    Worked on getting the nanosecond time - work in progress
    Signed-off-by: Dreaguns <jcr.engineer@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 package REMtracker.src;
 
+import REMtracker.src.PKGminiscope.MiniScope;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,6 +16,8 @@ public class Main extends Application {
         primaryStage.setTitle("REMtracker v1.0");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        MiniScope miniScope = new MiniScope("Hello", 1.0);
     }
 
 
--- src/PKGminiscope/MiniScopeTimerTask.java
+++ src/PKGminiscope/MiniScopeTimerTask.java
@@ -1,4 +1,12 @@
 package REMtracker.src.PKGminiscope;
+
+import javafx.animation.Animation;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+
+
 /**
  * MiniScopeTimerTask handles all time related items including following:
  * - keeps track of real-time
@@ -21,9 +29,32 @@ public class MiniScopeTimerTask {
   public int microsecond;
 
   //Constructor with no arguments
-    MiniScopeTimerTask(){}
+    MiniScopeTimerTask(){
+        getSystemTime();
+    }
 
     //Get the system time
-  public void getSystemTime(){}
+  public void getSystemTime(){
+      Date now = new Date();
+      SimpleDateFormat dataFormatter = new SimpleDateFormat("H"); //Military time: "H". Std: "h"
+      this.hour = Integer.parseInt(dataFormatter.format(now));
+      dataFormatter = new SimpleDateFormat("mm");
+      this.minute = Integer.parseInt(dataFormatter.format(now));
+      dataFormatter = new SimpleDateFormat("ss");
+      this.second = Integer.parseInt(dataFormatter.format(now));
+      dataFormatter = new SimpleDateFormat("ms");
+      this.millisecond = Integer.parseInt(dataFormatter.format(now));
+
+      System.out.println("NanoTime: " + System.nanoTime());
+      System.out.println("hour: " + hour);
+      System.out.println("minute: " + minute);
+      System.out.println("second: " + second);
+      System.out.println("millisecond: " + millisecond);
+      System.out.println("NanoTime: " + System.nanoTime());
+
+     // double nanoseconds = (System.nanoTime() - (this.hour*this.minute*this.second*this.millisecond)) ;
+     // System.out.println("Nanoseconds: " + nanoseconds);
+
+  }
 
 }
